8485c0f23fdbb92a14cda8f3e381e212d4392120
Jake Romer <jkrmr.io@gmail.com>
2017-04-01 14:42:31 -0700

    Add more attributes to Pokemon model / detail VC

--- Pokedex/Controller/PokemonDetailVC.swift
+++ Pokedex/Controller/PokemonDetailVC.swift
@@ -12,17 +12,30 @@ class PokemonDetailVC: UIViewController {
   @IBOutlet weak var pokemonName: UILabel!
   @IBOutlet weak var pokemonDescription: UILabel!
   @IBOutlet weak var pokemonDetailImage: UIImageView!
+  @IBOutlet weak var pokemonType: UILabel!
+  @IBOutlet weak var pokemonDefense: UILabel!
+  @IBOutlet weak var pokemonHeight: UILabel!
+  @IBOutlet weak var pokemonWeight: UILabel!
+  @IBOutlet weak var pokemonBaseAttack: UILabel!
+  @IBOutlet weak var pokemonPokedexId: UILabel!
 
   var pokemon: Pokemon!
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
     pokemonName.text = pokemon.name.capitalized
+    pokemonDescription.text = pokemon.description
+    pokemonType.text = pokemon.type
+    pokemonDefense.text = "\(pokemon.defense)"
+    pokemonHeight.text = "\(pokemon.height)"
+    pokemonWeight.text = "\(pokemon.weight)"
+    pokemonBaseAttack.text = "\(pokemon.baseAttack)"
+    pokemonPokedexId.text = "\(pokemon.pokedexId)"
     pokemonDetailImage.image = UIImage(named: "\(pokemon.pokedexId)")
-    pokemonDescription.text = "This is a Pokeman"
   }
 
   @IBAction func backButtonPressed(_ sender: Any) {
-    dismiss(animated: true, completion: nil)
+    dismiss(animated: true)
   }
 }
--- Pokedex/Model/Pokemon.swift
+++ Pokedex/Model/Pokemon.swift
@@ -9,19 +9,42 @@
 import Foundation
 
 class Pokemon {
+  let baseAttack: Int
+  let defense: Int
+  let description: String
+  let height: Int
   let name: String
   let pokedexId: Int
+  let type: String
+  let weight: Int
 
-  init(name: String, pokedexId: Int) {
-    (self.name, self.pokedexId) = (name, pokedexId)
+  init(name: String, pokedexId: Int, baseAttack: Int,
+       defense: Int, height: Int, weight: Int,
+       description: String = "", type: String = "") {
+    self.baseAttack = baseAttack
+    self.defense = defense
+    self.description = description
+    self.height = height
+    self.name = name
+    self.pokedexId = pokedexId
+    self.type = type
+    self.weight = weight
   }
 
   static func create(collectionFrom attrs: [Dictionary<String, String>]) -> [Pokemon] {
     var newCollection = [Pokemon]()
 
     for entry in attrs {
-      if let name = entry["identifier"], let idString = entry["id"], let id = Int(idString) {
-        let newPoke = Pokemon(name: name, pokedexId: id)
+      if let name = entry["identifier"],
+        let id = Int(entry["id"] ?? ""),
+        let height = Int(entry["height"] ?? ""),
+        let weight = Int(entry["weight"] ?? ""),
+        let base = Int(entry["exp"] ?? ""),
+        let defense = Int(entry["defense"] ?? "") {
+        let newPoke = Pokemon(name: name, pokedexId: id,
+                              baseAttack: base, defense: defense,
+                              height: height, weight: weight)
+
         newCollection.append(newPoke)
       }
     }
